9bc907b29163565e7927fd8189b0a292e06b312f
hassan <hassanmarzouk042@gmail.com>
2023-01-22 11:49:49 +0200

    flutter_food-app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:food_app/app/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const FoodApp());
 }
